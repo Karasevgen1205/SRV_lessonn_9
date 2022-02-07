be75c7360f5792cf7e5d4e80cd25c90e82434316
Yauheni Karas <yauhenikaras@ester.co>
2022-02-07 20:32:04 +0300

    init new project

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+// "usestrict";
+
+// const modalBtn = document.querySelector(".header__info_btn");
+// const modalWindow = document.querySelector(".modal-window");
+
+// modalBtn.addEventListener("click", () => {
+//   modalWindow.classList.add("modal-window__show");
+// });
+////////////////////////////////////////////////////////////
+
+// git --version - версия гита на компе
+// git init - инициализация пустого репозитория
